59ae00f6193ef039e87564a688577aec79326bb8
Khaos <khaos@khaos-coders.org>
2021-11-21 22:07:01 +0100

    Sample

---
+++ BindingProxyDemo/BindingProxy.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using Serilog;
+
+namespace BindingProxyDemo
+{
+    public class BindingProxy : Freezable
+    {
+        #region Overrides of Freezable
+
+        protected override Freezable CreateInstanceCore() => new BindingProxy();
+
+        #endregion
+
+        public object Data
+        {
+            get => this.GetValue(DataProperty);
+            set => this.SetValue(DataProperty, value);
+        }
+        public static readonly DependencyProperty DataProperty =
+            DependencyProperty.Register("Data", typeof(object), typeof(BindingProxy),
+                new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.AffectsRender, new PropertyChangedCallback(OnDataChanged)));
+
+        private static void OnDataChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            Log.Debug("Proxy data changed from {oldVal} to {newVal}", e.OldValue, e.NewValue);
+        }
+
+    }
+}
---
+++ BindingProxyDemo/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BindingProxyDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static readonly Random rnd = new Random();
+
+        public MainWindow()
+        {
+            InitLogging();
+            Log.Debug("Start App");
+
+            InitializeComponent();
+        }
+
+        private static void InitLogging() =>
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Debug()
+                .CreateLogger();
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Dispatcher is optional...
+            Dispatcher.BeginInvoke(new Action(() =>
+            {
+                Color color = Color.FromRgb((byte)rnd.Next(255), (byte)rnd.Next(255), (byte)rnd.Next(255));
+                SolidColorBrush brush = new SolidColorBrush(color);
+
+                Log.Debug("Changing color brush: {newColor}", color);
+                // 1. Try: set resource via global instance
+                //Application.Current.Resources["myBrush"] = brush;
+
+                // 2. Try: set resource in actual resource dictionary
+                Application.Current.Resources.MergedDictionaries[0]["myBrush"] = brush;
+
+                // 3. Try: set color on brush
+                //(Application.Current.Resources["myBrush"] as SolidColorBrush).Color = color;
+                // throws exception!
+            }));
+
+        }
+    }
+}
